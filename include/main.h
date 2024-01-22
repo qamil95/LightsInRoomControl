5295d123c41c66911441ded04a8ba240477e670b
Kamil Ziętek <qamil1995@gmail.com>
2024-01-22 19:04:00 +0100

    Added serial output with led state

--- include/main.h
+++ include/main.h
@@ -4,4 +4,8 @@
 unsigned long blockButtonsUntil = 0;
 const int timeOfButtonsBlock = 50;
 
+unsigned long doNotWriteDebugUntil = 0;
+const int timeOfDebugBlock = 1000;
+
 void changeLedState(byte pinNumber);
+void debugOutputState();
--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,8 @@ void setup() {
   pinMode(insideWardrobeButton, INPUT_PULLUP);
   pinMode(insideWardrobeOutput, OUTPUT);
   digitalWrite(insideWardrobeOutput, HIGH);
+
+  Serial.begin(9600);
   }
 
 void loop() {  
@@ -30,6 +32,8 @@ void loop() {
   {
     changeLedState(onDoorOutput);    
   }
+
+  debugOutputState();
 }
 
 void changeLedState(byte pinNumber)
@@ -41,4 +45,19 @@ void changeLedState(byte pinNumber)
       digitalWrite(pinNumber, !digitalRead(pinNumber));
   }
   blockButtonsUntil = currentTime + timeOfButtonsBlock;
+}
+
+void debugOutputState()
+{
+  unsigned long int currentTime = millis();
+  if (currentTime > doNotWriteDebugUntil)
+  {
+    Serial.print("MILLIS: ");
+    Serial.print(millis());
+    Serial.print(" LED STATE: ");
+    Serial.print(digitalRead(A0));
+    Serial.print(digitalRead(A1));
+    Serial.println(digitalRead(A2));
+    doNotWriteDebugUntil = currentTime + timeOfDebugBlock;
+  }
 }
